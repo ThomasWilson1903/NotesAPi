4e64b5fb559d23b2a0b4b706402328f03181aded
Александр Бирюков <hdnhdxv@gmail.com>
2023-06-20 00:39:27 +0400

    Initialize

---
+++ NotesPersistence/NotesDbContext.cs
@@ -0,0 +1,24 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using NotesApplication.Interfaces;
+using NotesDomain;
+using NotesPersistence.EntityTypeConfigurations;
+
+namespace NotesPersistence
+{
+    internal class NotesDbContext: DbContext, INotesDbContext
+    {
+        public DbSet<Note> Notes { get; set;}
+
+        public NotesDbContext(DbContextOptions<NotesDbContext> options)
+            : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfiguration(new NoteConfiguration());
+            base.OnModelCreating(builder);
+        }
+
+
+    }
+}
