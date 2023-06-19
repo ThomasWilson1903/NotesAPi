2a28b3f749c6c0a96a965226617881e7e65f41c4
Александр Бирюков <hdnhdxv@gmail.com>
2023-06-20 00:28:41 +0400

    Add project files.

---
+++ NotesDomain/Note.cs
@@ -0,0 +1,12 @@
+﻿namespace NotesDomain
+{
+    public class Note
+    {
+        public Guid UserId { get; set; }
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Detailse { get; set; }
+        public DateTime CreationDate { get; set; }
+        public DateTime EdirDate { get; set;}
+    }
+}
